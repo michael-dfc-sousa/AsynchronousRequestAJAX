dea4b693a764cbf8cd07ccac39dc8f4ff7e20542
Michael Douglas <michael.douglas_95@hotmail.com>
2024-12-21 23:06:04 -0300

    adicionando gb.json para criação de novas tarefas

---
+++ todoList/js/utils/createXMLHttpRequest.js
@@ -0,0 +1,20 @@
+export function createXMLHttRequest(method, url, cbfunction ,sendData = null){
+
+    const xhr = new XMLHttpRequest()
+    xhr.open(method, url)
+    xhr.send(sendData)
+    xhr.onreadystatechange = verificaAjax
+
+    function verificaAjax(){
+        if(xhr.readyState === 4){
+            if(xhr.status === 200 || xhr.status === 304){
+                if(typeof cbfunction === "function"){
+                    const json = JSON.parse(xhr.responseText)
+                    cbfunction(json)
+                }
+            }else if(typeof cbfunction === "function"){
+                cbfunction({"message":"algo deu errado: ", "status": xhr.status, "error": "algo deu errado"})
+            }
+        }
+    }
+}
